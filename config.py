d7331b87126038c1f552206c5f9e909f8f70827c
HuskyDS <57885045+MarioDS15@users.noreply.github.com>
2025-09-16 19:02:34 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Copy this file and add your actual token
 
 # Discord Bot Token
-DISCORD_TOKEN = "MTQxNzYyNjk0MjkzNjcxMTE4OQ.GeSrb-.Ixg8QCFPhmkCrHbd75tqZzo_OY9AY5ptrWOlKo"
+
 
 # Flask Settings
 FLASK_HOST = "0.0.0.0"
